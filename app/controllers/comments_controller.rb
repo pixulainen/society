ab05eb578da042afcfed84cd575b84a01c5518c9
simon <sjtucker28@gmail.com>
2020-02-13 15:24:06 +0000

    comments complete, so happy, about to start likes model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,15 @@
 class CommentsController < ApplicationController
     helper_method :current_user 
 
+    def destroy
+        @comment = Comment.find(params[:id])
+        @comment.destroy
+        # @comment.post = @post
+        # @post = @comment.post
+        @post = Post.find(params[:post_id])
+        redirect_to post_path(@post)
+    end
+
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(params[:comment].permit(:content, :user_id))
@@ -9,15 +18,32 @@ class CommentsController < ApplicationController
         redirect_to post_path(@post)
     end
 
-    def destroy
-        # @user.posts[2].comments[0].destroy
-        
+    def edit
         @post = Post.find(params[:post_id])
-        @comment = @post.comments.find(params[:id])
-        @comment.destroy
-        
-        redirect_to post_path(post)
+        @comment = Comment.find(params[:id])
+      end
+
+    def update 
+        @post = Post.find(params[:id])
+        @comment = Comment.find(params[:id])
+
+        if @comment.update(params[:comment].permit(:content))
+            redirect_to post_path(@post)
+        else 
+            render 'edit'
+        end
     end
+
+
+    # def destroy
+    #     # @user.posts[2].comments[0].destroy
+        
+    #     @post = Post.find(params[:post_id])
+    #     @comment = @post.comments.find(params[:id])
+    #     @comment.destroy
+        
+    #     redirect_to post_path(post)
+    # end
 
 
     private
